fc43150474d38163beeb50df9c09cbe4a3f0bff9
Unaixafaheem <233787@students.au.edu.pk>
2025-01-07 22:15:07 +0500

    Final_2

---
+++ Quzzing/Quzzing/Models/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace Quzzing.Models
+{
+    public class Question
+    {
+        public string QuestionTitle { get; set; }
+        public IEnumerable<string> Options { get; set; } = new List<string>();
+        public string Answer { get; set; } = string.Empty;
+    }
+}
